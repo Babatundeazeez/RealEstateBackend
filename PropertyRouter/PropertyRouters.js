04150fc0bd8505b20f6d6b6908b2e7dc3d196524
Babatundeazeez <techdevmind22@gmail.com>
2025-07-19 22:20:23 -0700

    cors-handler

--- PropertyRouter/PropertyRouters.js
+++ PropertyRouter/PropertyRouters.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { addProperty, displayProperty, getAllproperty, getSingleProperty, approveProperty, getAllApproveProperty, rejectProperty, getPendingProperty, getApproveProperty, disApprovedProperty } = require("../Controller/PropertyController")
+const {  displayProperty,  getSingleProperty, approveProperty,  getPendingProperty, getApproveProperty, disApprovedProperty } = require("../Controller/PropertyController")
 const upLoadImage = require("../Services/nodemailer/Multer")
 const isLoggedIn = require("../MiddleWare/UserVerification")
 const isAdmin = require("../MiddleWare/isAdmin")
@@ -7,28 +7,12 @@ const isAdmin = require("../MiddleWare/isAdmin")
 
 const propertyRouter = express.Router()
 
-
-
-
-
-
 //propertyRouter.post('/property', isLoggedIn, upLoadImage.single("image"), addProperty)
 propertyRouter.post('/',isLoggedIn, upLoadImage.single("image"), displayProperty)
 propertyRouter.patch('/approve/:id',isLoggedIn, isAdmin, approveProperty)
 propertyRouter.get('/approved', getApproveProperty)
 propertyRouter.get('/pending', isLoggedIn, isAdmin, getPendingProperty)
 propertyRouter.patch('/disapprove/:id',isLoggedIn, isAdmin, disApprovedProperty)
-
-//propertyRouter.get('/',getAllproperty)
-
-
-
-
-//propertyRouter.patch('/approved',isLoggedIn, isAdmin, approveProperty)
-
-
-//propertyRouter.get('/',getAllApproveProperty)
-
 
 propertyRouter.get('/:id', getSingleProperty)
 
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const connectToDB = require("./config/ConnectedDB");
 const authRouter = require("./userAuthRouterss/userAuthRouter");
-app = express()
+const userRouter = require("./userAuthRouterss/userRoutes");
+const BlogRouters = require("./PropertyRouter/BlogRouter");
+const GeneralRouter = require("./PropertyRouter/GeUserRouter");
+const propertyRouter = require("./PropertyRouter/PropertyRouters");
+const errorHandlers = require("./MiddleWare/ErrorHandler");
 
 const nigeriaStates = require("./utilities/nigeriaState.json")
 const propertyFile = require("./utilities/property.json")
-const propertyRouter = require("./PropertyRouter/PropertyRouters");
+
+const cors = require("cors");
+
+const app = express()
+
+connectToDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-
-const cors = require("cors");
 
 app.use(cors({
     origin:['http://localhost:5173','https://real-estate-projects-54plt8pav.vercel.app'],
@@ -18,24 +25,14 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(cors({origin: '*'}))
-
-const userRouter = require("./userAuthRouterss/userRoutes");
-
-const BlogRouters = require("./PropertyRouter/BlogRouter");
-const errorHandlers = require("./MiddleWare/ErrorHandler");
-const GeneralRouter = require("./PropertyRouter/GeUserRouter");
-
 
 ///connect to dbMongose//////////////
-connectToDB()
 const PORT = 1300;
 app.listen(PORT, ()=>{
     console.log("Listening to port " + PORT);
     
 })
-/////////////////////////////////////
-
+//////////Router///////////////////////////
 app.use('/api/auth', authRouter)
 app.use('/api/property', propertyRouter)
 app.use('/api/users',userRouter)
@@ -46,7 +43,7 @@ app.use('/api/generalUser', GeneralRouter)
 
 
 
-//Model View Controller Router
+//Model View  utility endpoint  Router
 app.get('/api/states', (req, res) =>{
     res.json(nigeriaStates)
 })
@@ -54,22 +51,20 @@ app.get('/api/property', (req, res)=>{
     res.json(propertyFile)
 })
 
+/////Allow header///////////////////
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+})
 
-
+/////Only 404 hander///////////
 app.use((req,res) =>{
     res.status(404).json({
         message : `${req.method} ${req.originalUrl} is not an end point to this server`
     })
 })
 
-app.use((req, res, next)=> {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
 
-app.use((req, res)=>{
-    res.status(404).json({message : "Route not Found"})
-})
 
 app.use(errorHandlers);
 
