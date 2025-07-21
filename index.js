43d51f2a94b9ac1a0a81a88b10df4ef14c17dde9
Babatundeazeez <techdevmind22@gmail.com>
2025-07-21 16:19:48 -0700

    Cors-update again

--- index.js
+++ index.js
@@ -23,18 +23,7 @@ const allowedOrigins =[
 
 ]
 
-app.use(cors({
-    origin: function( origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)){
-            callback(null, true);
-
-        }else{
-            callback (new Error("Not allowed byCORS"))
-        }
-    },
-    methods : ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+app.use(cors());
 
 
 connectToDB()
